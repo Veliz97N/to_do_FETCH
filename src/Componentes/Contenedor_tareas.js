efd88bbdd4e9f9ce2124970725b3dcfea5d7d5a3
Nicolas <nicolas.veliz@usach.cl>
2021-09-08 15:50:43 -0300

    Se añaden iconos a to do

--- src/Componentes/Contenedor_tareas.js
+++ src/Componentes/Contenedor_tareas.js
@@ -1,18 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { MdDelete } from "react-icons/md";
 
 function ContenedorTareas(props) {
-
     const handleTareas = () => {
-
         return props.tareas.map(tarea => (
-            <li  key={tarea.nombre}>{tarea.nombre}</li>       
+            <li key={tarea.nombre}>
+                {tarea.nombre}
+                <div className="Icono"><MdDelete /></div>
+            </li>
         ))
     }
-
     return (
         <div className="contenedor_tareas_externo">
             <div className="contenedor_tareas_interno">
-                    {handleTareas()}
+                {handleTareas()}
             </div>
         </div>
     )
