59b7d6456d4e271debd5a0ffb276cc96db5e12ee
Nicolas <nicolas.veliz@usach.cl>
2021-09-09 12:52:36 -0300

    Todo List terminadito

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import BarraSuperior from "./Componentes/Barra_superior"
 import FilaTareas from "./Componentes/FilaTareas";
 import FetchTareas from './FetchNoTanFetch/Listado_tareas';
@@ -9,48 +9,52 @@ function App() {
   const [user, setUser] = useState("Nicolas");
   const [ListaTareasGlobal, setListaTareas] = useState(FetchTareas());
   const [isTareasPendientes, setIsTareasPendientes] = useState(true);
-  
-  const GeneradorFilaTarea = () =>  {
-  return ListaTareasGlobal.map(tarea=>(!tarea.done && <FilaTareas tarea={tarea} key={tarea.nombre} toggleTarea={toggleTarea} visibilidad="Invisible"/> //Si el estado de tarea es falso entonces no se agregara esa tarea al div contenedor_tareas_interno
-      ))
+
+  const GeneradorFilaTarea = () => {
+    return ListaTareasGlobal.map(tarea => (!tarea.done && <FilaTareas handleDelete={handleDelete} handleApproved={handleApproved} tarea={tarea} key={tarea.nombre} toggleTarea={toggleTarea} visibilidad="Invisible" /> //Si el estado de tarea es falso entonces no se agregara esa tarea al div contenedor_tareas_interno
+    ))
   }
-  const GeneradorFilaTareaPendiente = () =>  {
-    return ListaTareasGlobal.map(tarea=>(tarea.done && <FilaTareas tarea={tarea} key={tarea.nombre} toggleTarea={toggleTarea} visibilidad="Visible" /> //Si el estado de tarea es falso entonces no se agregara esa tarea al div contenedor_tareas_interno
-        ))
-    }
-  const toggleTarea=(task)=>{
-    
-    setListaTareas(ListaTareasGlobal.map(tarea=>{
-      let bool=tarea.done
-      return(tarea.nombre===task.nombre?{...tarea,done:!bool}:tarea)}));    //ACA SOLO LE ESTAMOS DICIENDO TRUE
-    if(ListaTareasGlobal.filter(t=>!t.done).length>1){ //Esto es para ver si hay tareas pendientes, nada mas 
+  const GeneradorFilaTareaPendiente = () => {
+    return ListaTareasGlobal.map(tarea => (tarea.done && <FilaTareas handleDelete={handleDelete} handleApproved={handleApproved} tarea={tarea} key={tarea.nombre} toggleTarea={toggleTarea} visibilidad="Visible" /> //Si el estado de tarea es falso entonces no se agregara esa tarea al div contenedor_tareas_interno
+    ))
+  }
+
+  const toggleTarea = (task) => {
+    setListaTareas(ListaTareasGlobal.map(tarea => {
+      return (tarea.nombre === task.nombre ? { ...tarea, done: true } : tarea)
+    }));
+    if (ListaTareasGlobal.filter(t => !t.done).length >= 1) { 
       setIsTareasPendientes(true)
     }
-    else{
+    else {
       setIsTareasPendientes(false)
     }
   }
-  const handleApproved=()=>{    
-    console.log("Checked papi")
+  const handleApproved = (task) => {
+    setListaTareas(ListaTareasGlobal.map(tarea => {
+      return (tarea.nombre === task.nombre ? { ...tarea, done: false } : tarea)
+    }));
   }
-  const handleDelete=()=>{
-    console.log("Eliminada mi rey")
+
+  const handleDelete = (task) => {
+    const nuevaListaTareasGlobal= ListaTareasGlobal.filter((tarea)=> tarea.nombre!==task.nombre)
+    setListaTareas(nuevaListaTareasGlobal);
   }
-  const AgregarNuevaTarea=(nombreTarea)=>{//ACA SE AGREGA UNA NUEVA LISTA PARA CADA TAREA PENDIENTE
-    if(!ListaTareasGlobal.find(tarea=>tarea.nombre===nombreTarea)){
-      setListaTareas([...ListaTareasGlobal,{nombre:nombreTarea, done:false}])
+  const AgregarNuevaTarea = (nombreTarea) => {//ACA SE AGREGA UNA NUEVA LISTA PARA CADA TAREA PENDIENTE
+    if (!ListaTareasGlobal.find(tarea => tarea.nombre === nombreTarea)) {
+      setListaTareas([...ListaTareasGlobal, { nombre: nombreTarea, done: false }])
     }
-  }  
+  }
 
   return (
     <div className="App">
       <BarraSuperior usuario={user} ListaTareas={ListaTareasGlobal} />
       <div className="contenedor_tareas_externo">
-          <AgregarTareas AgregarNuevaTarea={AgregarNuevaTarea} setIsTareasPendientes={setIsTareasPendientes}/>
+        <AgregarTareas AgregarNuevaTarea={AgregarNuevaTarea} setIsTareasPendientes={setIsTareasPendientes} />
         <div className="contenedor_tareas_interno">
-          {isTareasPendientes ? GeneradorFilaTarea():<p id="NoHayTareas">No tasks, add a task</p>}
+          {isTareasPendientes ? GeneradorFilaTarea() : <p id="NoHayTareas">No tasks, add a task</p>}
         </div>
-        <TareasCompletas pasarTareaAPendientes={handleApproved} eliminarTareaDefinitivamente={handleDelete} ListaTareasGlobal={ListaTareasGlobal} GeneradorFilaTareasPendientes={GeneradorFilaTareaPendiente()}/>
+        <TareasCompletas pasarTareaAPendientes={handleApproved} eliminarTareaDefinitivamente={handleDelete} ListaTareasGlobal={ListaTareasGlobal} GeneradorFilaTareasPendientes={GeneradorFilaTareaPendiente()} />
       </div>
     </div>
   );
--- src/Componentes/FilaTareas.js
+++ src/Componentes/FilaTareas.js
@@ -5,14 +5,19 @@ import { AiFillCheckCircle } from "react-icons/ai";
 
 function Tareas(props) {
     const [isShow, setIsShow] = useState(false)  
+
     return (<li key={props.tarea.nombre}
             onMouseOver={()=>setIsShow(true)}
             onMouseLeave={()=>setIsShow(false)}>
         {props.tarea.nombre} 
-            <FaTimes  className={isShow ? "Icono Visible":"Icono Invisible"} 
+            <FaTimes  className={props.visibilidad==="Invisible"?(isShow ? "Icono Visible":"Icono Invisible"):(isShow ? "Icono Invisible":"Icono Invisible")} 
             onClick={()=>props.toggleTarea(props.tarea)}/> {/*Icono para marcar tarea como lista*/}
-            <AiFillCheckCircle className={"ticket " + props.visibilidad} onClick={()=>props.toggleTarea(props.tarea)}/> {/*Icono para marcar tarea como pendiente desde la ventana de listas xde pasarTareaAPendientes*/}
-            <MdDelete className={"ticket " + props.visibilidad}/> {/*Icono Para eliminar tarea de la lista eliminarTareaDefinitivamente*/}
+            
+            <AiFillCheckCircle className={"ticket " + props.visibilidad} 
+            onClick={()=>props.handleApproved(props.tarea)}/> {/*Icono para marcar tarea como pendiente desde la ventana de listas xde pasarTareaAPendientes*/}
+            
+            <MdDelete className={"ticket " + props.visibilidad}
+            onClick={()=>props.handleDelete(props.tarea)}/> {/*Icono Para eliminar tarea de la lista eliminarTareaDefinitivamente*/}
     </li>  );
 }
 export default Tareas;
